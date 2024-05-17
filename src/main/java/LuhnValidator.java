51481d5637964dd078841130dbd70effc0633c88
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-17 16:20:24 -0300

    Exercise solution

--- src/main/java/LuhnValidator.java
+++ src/main/java/LuhnValidator.java
@@ -1,7 +1,26 @@
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        candidate = candidate.replace(" ", "");
+        if (candidate.length() < 2) return false;
+
+        final var reversedString = new StringBuilder(candidate).reverse().toString();
+        return calculateLuhnChecksum(reversedString) % 10 == 0;
+    }
+
+    private static int calculateLuhnChecksum(String reversedString) {
+        IntUnaryOperator getDoubledOrOriginalDigit = index -> {
+            int numberValue = Character.digit(reversedString.charAt(index), 10);
+
+            return index % 2 == 0 ? numberValue : (numberValue * 2 > 9 ? numberValue * 2 - 9 : numberValue * 2);
+        };
+
+        return IntStream.range(0, reversedString.length())
+                .map(getDoubledOrOriginalDigit)
+                .sum();
     }
 
 }
